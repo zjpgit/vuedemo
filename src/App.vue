cf87dc1f22bad316f5374f5843240e81321bb1fb
zjpgit <646527950@qq.com>
2019-12-02 16:15:15 +0800

    提交第一个vue测试程序

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <ul>
+      <tree :model="data">
+      </tree>
+    </ul>
+  </div>
+</template>
+
+<script>
+import tree from "./components/tree.vue"
+
+export default {
+  name: 'App',
+  components: {
+    tree
+  },
+  data () {
+    return {
+      data: data
+    }
+  }
+}
+
+var data = {
+  name: 'My Tree',
+  children: [
+    { name: 'hello' },
+    { name: 'wat' },
+    {
+      name: 'child folder',
+      children: [
+        {
+          name: 'child folder',
+          children: [
+            { name: 'hello' },
+            { name: 'wat' }
+          ]
+        },
+        { name: 'hello' },
+        { name: 'wat' },
+        {
+          name: 'child folder',
+          children: [
+            { name: 'hello' },
+            { name: 'wat' }
+          ]
+        }
+      ]
+    }
+  ]
+}
+</script>
+
+<style>
+</style>
